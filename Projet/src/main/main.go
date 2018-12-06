ff144a60a533869d802eef38fa64f441ac8b18df
Zakitos <zakaria.aboham@gmail.com>
2018-12-06 19:46:42 +0100

    TCCHAT

--- Projet/src/main/main.go
+++ Projet/src/main/main.go
@@ -3,14 +3,17 @@ package main
 import (
   "fmt"
   "net/http"
+  "strings"
 )
 const SEND_MESSAGE = len("/MESSAGE/")
-func handler (w http.ResponseWriter, r *http.Request){
-  title := string(r.URL.Path[SEND_MESSAGE:])
-  fmt.Println()
+func handler_message (w http.ResponseWriter, r *http.Request){
+  message_received := r.URL.Path[SEND_MESSAGE:]
+  fmt.Println(message_received)
+  message := strings.Split(message_received," ")
+  fmt.Println(message)
 }
 
 func main (){
-  http.HandleFunc("/MESSAGE/",handler) // Si on a une requete sur le localhost:8080/ on fait handler
-  http.ListenAndServe(":8080", nil) // On lance le serveur, on écoute en boucle
+  http.HandleFunc("/MESSAGE/",handler_message) // Si on a une requete sur le localhost:8080/ on fait handler
+  http.ListenAndServe(":8080",nil) // On lance le serveur, on écoute en boucle
 }
